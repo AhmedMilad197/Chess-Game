9433089198092ee51a7598298ad03038c804c385
AhmedMilad197 <ahmilad197@gmail.com>
2024-08-21 20:20:48 +0200

    added axios package

--- Client/src/components/SignUp.vue
+++ Client/src/components/SignUp.vue
@@ -13,7 +13,7 @@
           <input
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email" type="text" placeholder="Email">
-          <p class="text-gray-500 text-xs italic">Give us your email.</p>
+          <p class="text-gray-500 text-xs">Give us your email.</p>
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
@@ -22,16 +22,16 @@
           <input
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username" type="text" placeholder="Username">
-          <p class="text-gray-500 text-xs italic">Please choose a username.</p>
+          <p class="text-gray-500 text-xs">Please choose a username.</p>
         </div>
         <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          <label class="block text-gray-700 text-sm font-bold" for="password">
             Password
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password" type="password" placeholder="Password">
-          <p class="text-gray-500 text-xs italic">Please choose a password.</p>
+          <p class="text-gray-500 text-xs">Please choose a password.</p>
         </div>
         <div class="flex items-center justify-around">
           <button
